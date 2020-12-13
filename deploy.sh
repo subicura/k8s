0e5f4624b37b933ec3dd93ff79c4bf527a99f9c7
Chungsub Kim <chungsubkim@kolon.com>
2020-12-14 04:11:41 +0900

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist/k8s
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:subicura/k8s.git master:gh-pages
+
+cd -
