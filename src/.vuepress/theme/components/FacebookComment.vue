f5306986cf876bed4285fd8850db00217668b76a
Chungsub Kim <chungsubkim@kolon.com>
2020-12-26 15:23:39 +0900

    update fb comment

--- src/.vuepress/theme/components/FacebookComment.vue
+++ src/.vuepress/theme/components/FacebookComment.vue
@@ -1,52 +1,59 @@
 <template>
-<div class="fb-comment-wrapper">
-  <div class="fb-comment-inner">
-    <div class="fb-comments" :data-href="commentLink" data-num-posts="5" data-order-by="social" data-width="100%"></div>
+  <div class="fb-comment-wrapper">
+    <div class="fb-comment-inner">
+      <div
+        class="fb-comments"
+        :data-href="commentLink"
+        data-num-posts="5"
+        data-order-by="social"
+        data-width="100%"
+      ></div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
-  name: 'FacebookComment',
-   watch: {
-    '$route' (to, from) {
-      if (
-        to.path !== from.path
-      ) {
-        if (this.$el.querySelector('div.fb-comments')) {
-          this.$el.querySelector('div.fb-comments').innerHTML = '';
+  name: "FacebookComment",
+  watch: {
+    $route(to, from) {
+      if (to.path !== from.path) {
+        if (this.$el.querySelector("div.fb-comments")) {
+          this.$el.querySelector("div.fb-comments").innerHTML = "";
         }
         setTimeout(() => {
           this.load();
         }, 500);
       }
-    }
-  }, 
-  mounted () {
+    },
+  },
+  mounted() {
     this.load();
-  }, 
+  },
   methods: {
     load() {
-      if (typeof FB !== 'undefined') {
+      if (typeof FB !== "undefined") {
         FB.XFBML.parse();
       }
-    }
+    },
   },
   computed: {
     commentLink() {
       var relativePath = this.$page.relativePath;
-      var path = relativePath.replace('.md', '.html');
-      
+      var path = relativePath.replace(".md", ".html");
+
       // blog(_archive, _news) 일때 처리
       if (relativePath.indexOf("_archive") > -1 || path.indexOf("_news") > -1) {
         path = this.$page.path.substring(1);
       }
 
-      return 'https://subicura.com/k8s/' + this.$page.relativePath.replace('.md', '.html');
-    }
+      return (
+        "https://subicura.com/k8s/" +
+        this.$page.relativePath.replace(".md", ".html")
+      );
+    },
   },
-}
+};
 </script>
 
 <style lang="stylus">
@@ -55,6 +62,7 @@ export default {
   margin: 0 auto
   padding: 1.5rem 2rem
   margin 0 auto
+  z-index 1
   @media (max-width: $MQNarrow)
     padding 1.5rem
   @media (max-width: $MQMobileNarrow)
@@ -65,5 +73,4 @@ export default {
 .yuu-theme-dark
   .fb-comment-inner
     background-color: #f3f3f3
-
 </style>
