e14af59c0571092ec655e978106ae789f5f57c77
subicura <subicura@subicura.com>
2021-07-14 13:48:26 +0900

    update main branch

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:subicura/k8s.git master:gh-pages
+git push -f git@github.com:subicura/k8s.git main:gh-pages
 
 cd -
