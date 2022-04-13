5bb5b457b20f3430d10b0d88a630a6614fc2a27c
subicura <subicura@subicura.com>
2022-04-13 09:29:07 +0900

    deploy

---
+++ assets/js/21.6969edd2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{452:function(t,s,n){"use strict";n.r(s);var e={props:["text"],data:function(){return{msg:this.text}}},i=n(17),a=Object(i.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("p",{staticClass:"usage-msg"},[this._v("\n  "+this._s(this.msg)+"\n")])}),[],!1,null,null,null);s.default=a.exports}}]);
